d4c6dba3df7a67e1bc53fb8505da9f59c2760e3a
toothlessdev <toothless042@gmail.com>
2024-05-22 15:24:14 +0900

    chore: PostgreSQL 에서 Sqlite 로 DBMS 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { TypeOrmRootModule } from './config/db.config';
+import {
+  TypeOrmRootModule_Develop,
+  TypeOrmRootModule_Production,
+} from './config/db.config';
 import { RootConfigModule } from './config/root.config';
 import { DiagnosisModule } from './diagnosis/diagnosis.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, TypeOrmRootModule, RootConfigModule, DiagnosisModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    process.env.NODE_ENV === 'dev'
+      ? TypeOrmRootModule_Develop
+      : TypeOrmRootModule_Production,
+    RootConfigModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -5,13 +5,13 @@ import { UserModel } from 'src/users/entities/user.entity';
 
 const RootEntities = [UserModel, DiagnosisModel];
 
-export const TypeOrmRootModule = TypeOrmModule.forRootAsync({
+export const TypeOrmRootModule_Production = TypeOrmModule.forRootAsync({
   imports: [ConfigModule],
   useFactory: (configService: ConfigService) => ({
     type: 'sqlite',
-    // port: 5432,
-    // username: configService.get<string>('DB_USER_NAME'),
-    // password: configService.get<string>('DB_PASSWORD'),
+    port: 5432,
+    username: configService.get<string>('DB_USER_NAME'),
+    password: configService.get<string>('DB_PASSWORD'),
     database: 'database.db',
     entities: RootEntities,
     synchronize: true,
@@ -19,7 +19,7 @@ export const TypeOrmRootModule = TypeOrmModule.forRootAsync({
   inject: [ConfigService],
 });
 
-export const TypeOrmRootModule = TypeOrmModule.forRoot({
+export const TypeOrmRootModule_Develop = TypeOrmModule.forRoot({
   type: 'sqlite',
   database: 'database.db',
   entities: RootEntities,
