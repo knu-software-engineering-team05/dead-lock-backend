d749a59f767013c19f4870279d3f0a9d5fd8b618
toothlessdev <toothless042@gmail.com>
2024-06-05 04:45:36 +0900

    refactor: Flask, NestJS 서버 컨테이너화

--- server/src/diagnosis/diagnosis.service.ts
+++ server/src/diagnosis/diagnosis.service.ts
@@ -77,7 +77,7 @@ export class DiagnosisService {
     user: UserModel,
     createDiagnosisReportRequestDto: CreateDiagnosisRequestDto,
   ) {
-    const response = await fetch('http://127.0.0.1:5000/predict', {
+    const response = await fetch("http://127.0.0.1:5000/predict", {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
