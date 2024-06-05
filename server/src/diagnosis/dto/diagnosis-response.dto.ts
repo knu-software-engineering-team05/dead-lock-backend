a143f393d03307c982be0ab0922593c289ad3089
toothlessdev <toothless042@gmail.com>
2024-06-05 14:06:42 +0900

    refactor: DiagnosisResponseDto CamelCase 로 수정

--- server/src/diagnosis/dto/diagnosis-response.dto.ts
+++ server/src/diagnosis/dto/diagnosis-response.dto.ts
@@ -11,24 +11,24 @@ export class DiagnosisReportResponseDto {
   score: number;
 
   @ApiProperty({ example: 0 })
-  stroke_probability: number;
+  strokeProbability: number;
 
   @ApiProperty({ example: '진단결과' })
-  total_diagnosis: string;
+  totalDiagnosis: string;
 
   @ApiProperty({ example: '식습관 개선방안' })
-  eating_habits: string;
+  eatingHabits: string;
 
   @ApiProperty({ example: '생활습관 개선방안' })
-  lifestyle_habits: string;
+  lifestyleHabits: string;
 
   constructor(diagnosisReport) {
     this.id = diagnosisReport.id;
     this.createdAt = diagnosisReport.createdAt;
     this.score = diagnosisReport.score;
-    this.stroke_probability = diagnosisReport.stroke_probability;
-    this.total_diagnosis = diagnosisReport.total_diagnosis;
-    this.eating_habits = diagnosisReport.eating_habits;
-    this.lifestyle_habits = diagnosisReport.lifestyle_habits;
+    this.strokeProbability = diagnosisReport.stroke_probability;
+    this.totalDiagnosis = diagnosisReport.total_diagnosis;
+    this.eatingHabits = diagnosisReport.eating_habits;
+    this.lifestyleHabits = diagnosisReport.lifestyle_habits;
   }
 }
